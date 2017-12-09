72d62a69e3df5d55e62bdfb833ace98be60989c6
Joseph A. Daly <josefdaly@gmail.com>
2017-12-09 17:17:19 -0500

    comment clean up

--- lib/chess_utils.py
+++ lib/chess_utils.py
@@ -6,8 +6,6 @@ PLAYER_COMPUTER = 'computer'
 
 
 # super simple for now
-# ideally, this will be performed by a smart third party AI, because I don't really want to
-#   implement a Chess AI from scratch. This will be fine for now.
 def make_ai_move(board):
     move = random.choice([move.uci() for move in board.legal_moves])
     board.push_san(move)
@@ -18,13 +16,6 @@ def process_moves(moves, board):
     for move in moves:
         board.push_san(move)
     return board
-    # if not board.is_game_over():
-    #     move = random.choice([move.uci() for move in board.legal_moves])
-    #     board.push_san(move)
-    #     if board.is_game_over():
-    #         return 'I win'
-    #     return move
-    # return 'you win'
 
 
 def run_game(moves):
