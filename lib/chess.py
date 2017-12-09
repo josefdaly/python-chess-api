7b72cb00c0a892f60cd09f0e95205bc6732e4ded
Joseph A. Daly <josefdaly@gmail.com>
2017-12-09 11:17:51 -0500

    basic chess utilities

---
+++ lib/chess.py
@@ -0,0 +1,39 @@
+import chess, random
+
+
+PLAYER_HUMAN = 'player'
+PLAYER_COMPUTER = 'computer'
+
+
+# super simple for now
+# ideally, this will be performed by a smart third party AI, because I don't really want to
+#   implement a Chess AI from scratch. This will be fine for now.
+def make_ai_move(board):
+    move = random.choice([move.uci() for move in board.legal_moves])
+    board.push_san(move)
+    return board, move
+
+def process_moves(moves, board):
+    for move in moves:
+        board.push_san(move)
+    return board
+    # if not board.is_game_over():
+    #     move = random.choice([move.uci() for move in board.legal_moves])
+    #     board.push_san(move)
+    #     if board.is_game_over():
+    #         return 'I win'
+    #     return move
+    # return 'you win'
+
+
+def run_game(moves):
+    try:
+        board = process_moves(moves, chess.Board())
+    except ValueError:
+        return 'error'
+    if board.is_game_over():
+        return {'move': None, 'winner': PLAYER_HUMAN}
+    board, move = make_ai_move(board)
+    if board.is_game_over():
+        return {'move': move, 'winner': PLAYER_COMPUTER}
+    return {'move': move, 'winner': None}
---
+++ tests/test_chess.py
@@ -0,0 +1,14 @@
+import pytest, chess
+
+from lib.chess import process_moves
+
+
+class TestProcessMoves(object):
+
+    def test_process_moves_basic_case(self):
+        processed_board = process_moves(['e2e4'], chess.Board())
+
+        control_board = chess.Board()
+        control_board.push_san('e2e4')
+
+        assert sorted([move.uci() for move in processed_board.legal_moves]) == sorted([move.uci() for move in control_board.legal_moves])
